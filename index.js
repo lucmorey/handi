750b1acbe02497b53a1124ab041db388f0ff78f3
David Sersch <dsersch640@gmail.com>
2017-10-16 10:26:29 -0700

    initial server setup

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-//It comes in handi!
+const
+    express = require('express'),
+    app = express(),
+    ejsLayouts = require('express-ejs-layouts'),
+	mongoose = require('mongoose'),
+	flash = require('connect-flash'),
+	logger = require('morgan'),
+	cookieParser = require('cookie-parser'),
+	bodyParser = require('body-parser'),
+	session = require('express-session'),
+	MongoDBStore = require('connect-mongodb-session')(session),
+    passport = require('passport'),
+    port = 3000,
+    mongoConnectionString = 'mongodb://localhost/handi'
+ 
+mongoose.connect(mongoConnectionString, (err)=>{
+    console.log(err || 'Connected to Database. 👍')
+})    
+
+app.listen(port, (err)=>{
+    console.log(err || `Server Running on port ${port}. 👍`)
+})
+
+app.get('/', (req, res)=>{
+    res.send('homepage working')
+})
