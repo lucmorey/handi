ae6b258f0adadd1eec754e084f5fbb1f141e9b7c
David Sersch <dsersch640@gmail.com>
2017-10-16 13:33:46 -0700

    users controller, users views

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 const
     express = require('express')
     usersRouter = new express.Router()
+    usersCntr = require('../controllers/users.js')
 
 usersRouter.route('/login')
     .get((req, res)=>{
         res.render('login')
     })
+
 
 module.exports = usersRouter
